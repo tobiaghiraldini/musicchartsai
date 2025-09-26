9cb94028309f253ecf3b203a4bfc0d6ec8aca9f8
Tobia Ghiraldini <tobia.ghiraldini@ninjabit.com>
2025-09-26 12:23:41 +0200

    fix: potential duplicate platforms contraint

--- apps/soundcharts/models.py
+++ apps/soundcharts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Platform(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255, unique=True)
     
     # Enhanced platform metadata
     platform_type = models.CharField(max_length=50, choices=[
